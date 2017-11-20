a05cc4ba6d7ab6da2506f40e343d3882240ca7ab
Filip Lux <filip.lux@comprimato.com>
2017-11-20 19:18:57 +0100

    rename facade files

--- service/src/main/java/cz/muni/fi/pa165/soccermanager/service/facade/LeagueFacadeImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/soccermanager/service/facade/LeagueFacadeImpl.java
@@ -4,5 +4,5 @@ package cz.muni.fi.pa165.soccermanager.service.facade;
  * @author 445720 Martin Hamernik
  * @version 11/16/2017.
  */
-public class LeagueFacade {
+public class LeagueFacadeImpl {
 }
