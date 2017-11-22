71b862dba21ddd2498ab8daf35264e1e9e20a003
Iman Mehmandoust <imanmehmandoust.cs@gmail.com>
2017-11-22 16:24:10 +0100

    Impl of LeagueDao, LeagueDaoImpl and LeagueFacadeImpl.java files

--- persistence/src/main/java/cz.muni.fi.pa165.soccermanager/dao/LeagueDao.java
+++ persistence/src/main/java/cz.muni.fi.pa165.soccermanager/dao/LeagueDao.java
@@ -7,15 +7,57 @@ import java.util.List;
 
 /**
  * @author 476368 Iman Mehmandoust
- * @version 10/27/2017.
+ * @version 11/22/2017.
  * this interface is implemented in leagueDaoImpl class
  */
 
 
 public interface LeagueDao {
+
+    /**
+     * Finds league in db by id.
+     * @param leagueId id of desired team
+     * @return league retrieved from db
+     */
+
     League fetchById(long leagueId);
+
+    /**
+     * Retrieves all leagues from db
+     * @return List of leagues retrieved from db
+     */
     List<League> fetchAll();
+
+    /**
+     * Retrieves team matching provided name from db
+     * @param name name of league
+     * @return league retrieved from db, null if no such league is in db
+     */
+    League fetchByName(String name);
+
+    /**
+     * Retrieves league matching provided country from db
+     * @param country name of league
+     * @return league retrieved from db, null if no such league is in db
+     */
+    League fetchByCountry(String country);
+
+    /**
+     * Inserts new league to db
+     * @param league instance of league which shall be inserted
+     *
+     */
     void insert(League league);
+
+    /**
+     * Finds league by id and updates it with new attributes.
+     * @param league league already stored in db which shall be updated
+     */
     void update(League league);
+
+    /**
+     * Finds league by id and deletes it from db
+     * @param leagueId id of league that shall be deleted
+     */
     void delete(long leagueId);
 }
--- service/src/main/java/cz/muni/fi/pa165/soccermanager/service/facade/LeagueFacadeImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/soccermanager/service/facade/LeagueFacadeImpl.java
@@ -1,8 +1,38 @@
 package cz.muni.fi.pa165.soccermanager.service.facade;
 
+import cz.muni.fi.pa165.soccermanager.dto.LeagueDTO;
+import cz.muni.fi.pa165.soccermanager.enums.NationalityEnum;
+import cz.muni.fi.pa165.soccermanager.facade.LeagueFacade;
+
+import java.util.List;
+
 /**
  * @author 445720 Martin Hamernik
  * @version 11/16/2017.
  */
-public class LeagueFacadeImpl {
+public class LeagueFacadeImpl implements LeagueFacade {
+    @Override
+    public List<LeagueDTO> getLeaguesByCountry(NationalityEnum country) {
+        return null;
+    }
+
+    @Override
+    public LeagueDTO getLeagueById(Long id) {
+        return null;
+    }
+
+    @Override
+    public LeagueDTO getLeagueByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void addMatch(Long leagueId, Long matchId) {
+
+    }
+
+    @Override
+    public void simulateMatchday() {
+
+    }
 }
