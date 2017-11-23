9713609d958352e13509c7c780cf972cbf007934
Filip Lux <filip.lux@comprimato.com>
2017-11-23 18:07:03 +0100

    MatchService imlementation

--- service/src/main/java/cz/muni/fi/pa165/soccermanager/service/MatchService.java
+++ service/src/main/java/cz/muni/fi/pa165/soccermanager/service/MatchService.java
@@ -1,17 +1,24 @@
 package cz.muni.fi.pa165.soccermanager.service;
 
 import cz.muni.fi.pa165.soccermanager.entity.Match;
+import cz.muni.fi.pa165.soccermanager.entity.Team;
+import cz.muni.fi.pa165.soccermanager.enums.StadiumEnum;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
- * @author 445720 Martin Hamernik
- * @version 11/16/2017.
+ * An interface that defines a service access to the Match entity
+ * @author 456519 Filip Lux
+ * @version 11/23/2017.
  */
 public interface MatchService {
-    Match fetchById(long matchId);
-    List<Match> fetchAll();
-    void insert(Match match);
+    Match findById(long matchId);
+    List<Match> findAll();
+    List<Match> findByDate(LocalDate date);
+    List<Match> findByTeam(Team team);
+    List<Match> findByStadium(StadiumEnum stadium);
+    void create(Match match);
     void update(Match match);
-    void delete(long matchId);
+    void remove(Long matchId);
 }
